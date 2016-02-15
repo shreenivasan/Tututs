b0613177b3d0923ad35b0b4b524b3f282a6f5d25
shreenivas <shreenivas@lp-0441>
2016-02-15 15:01:41 +0530

    Added common functionality file like xss_clen,site_url,csv export etc.

---
+++ Functions/index.php
@@ -0,0 +1,43 @@
+<?php
+
+function csv_export($data, $filename, $attachment = true, $headers = true)
+{		
+        //print_r(array_keys($data[0]));
+        //mb_internal_encoding("UTF-8");	
+        if(count($data) > 0){
+                if($attachment) {
+            // send response headers to the browser
+            header( 'Content-Type: text/csv; charset=utf-8' );	            
+            header( 'Content-Disposition: attachment;filename='.$filename);
+            $fp = fopen('php://output', 'w');
+        } else {
+            $fp = fopen($filename, 'w');
+        }
+
+        fputcsv($fp, array_keys($data[0]));
+                foreach ($data as $rows) {				
+                        fputcsv($fp, $rows);
+                }
+
+                fclose($fp);		
+        }
+
+}
+
+function site_url($url = '')
+{
+        if(empty($url)){
+                return ROOT_URL;
+        }else{
+                return ROOT_URL.'/'.$url;
+        }
+}
+        
+function xss_encode($string, $charset = 'UTF-8')
+{
+        $replace = "";
+        $search = array('{','}','(',')');
+        $string = str_ireplace($search, $replace, $string);
+        $string = htmlentities($string, ENT_QUOTES, $charset);
+        return $string;
+}        
