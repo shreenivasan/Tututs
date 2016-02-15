72bf83e7de166c2cec3e41d69c1abe8b2bd8deb0
shreenivas <shreenivas@lp-0441>
2016-02-15 14:48:34 +0530

    Redis tutorials added

---
+++ Redis/list.php
@@ -0,0 +1,15 @@
+<?php
+
+   //Connecting to Redis server on localhost
+   $redis = new Redis();
+   $redis->connect('127.0.0.1', 6379);
+   echo "Connection to server sucessfully";
+   //store data in redis list
+   $redis->lpush("tutorial-list", "Redis");
+   $redis->lpush("tutorial-list", "Mongodb");
+   $redis->lpush("tutorial-list", "Mysql");
+   // Get the stored data and print it
+   $arList = $redis->lrange("tutorial-list", 0 ,5);
+   echo "Stored string in redis:: ";
+   print_r($arList);
+
---
+++ Redis/string.php
@@ -0,0 +1,10 @@
+<?php
+
+   //Connecting to Redis server on localhost
+   $redis = new Redis();
+   $redis->connect('127.0.0.1', 6379);
+   echo "Connection to server sucessfully";
+   //set the data in redis string
+   $redis->set("tutorial-name", "Redis tutorial");
+   // Get the stored data and print it
+   echo "Stored string in redis:: " + jedis.get("tutorial-name");
