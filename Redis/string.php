035cbb09b715797c5a4581c4156bef7711b67044
shreenivas <shreenivas@lp-0441>
2016-02-19 16:07:47 +0530

    changed string in Redis | used get & set method

--- Redis/string.php
+++ Redis/string.php
@@ -1,10 +1,14 @@
 <?php
-
    //Connecting to Redis server on localhost
    $redis = new Redis();
    $redis->connect('127.0.0.1', 6379);
    echo "Connection to server sucessfully";
    //set the data in redis string
-   $redis->set("tutorial-name", "Redis tutorial");
+   if(empty($redis->get("tutorial-name")))
+   {
+       $redis->set("tutorial-name", "Redis tutorial 2");
+   }
+   
+   
    // Get the stored data and print it
-   echo "Stored string in redis:: " + jedis.get("tutorial-name");
+   echo "<br>Stored string in redis:: ".$redis->get("tutorial-name");
