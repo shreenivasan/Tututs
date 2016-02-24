18528b1bcd9a38c7c6ea37e64945dfbfaea0229c
shreenivas <shreenivas@lp-0441>
2016-02-24 14:57:24 +0530

    remote image exists or not checking using php

--- image_exists.php
+++ image_exists.php
@@ -19,5 +19,5 @@ else {
 
 $file='http://i1.pepperfry.com/media/catalog/product/c/a/800x400/canon-red-recliner-by-alcanes-canon-red-recliner-by-alcanes-u6yqbb.jpg';
 $file2='http://i1.pepperfry.com/media/catalog/product/c/a/800x880/canon-red-recliner-by-alcanes-canon-red-recliner-by-alcanes-u6yqbb.jpg';
-echo remote_file_exists($file);
-echo remote_file_exists($file2);
+echo remote_file_exists($file)?"Y":"N";
+echo remote_file_exists($file2)?"Y":"N";
