5ea5e3ef666586c6ba24c8c20eab6fdea3bee45c
shreenivas <shreenivas@lp-0441>
2016-02-24 12:35:11 +0530

    check remote php file exists or not

---
+++ image_exists.php
@@ -0,0 +1,23 @@
+<?php
+
+function remote_file_exists($file){
+
+$url=getimagesize($file);
+
+if(is_array($url))
+{
+
+ return true;
+
+}
+else {
+
+ return false;
+
+}
+}
+
+$file='http://i1.pepperfry.com/media/catalog/product/c/a/800x400/canon-red-recliner-by-alcanes-canon-red-recliner-by-alcanes-u6yqbb.jpg';
+$file2='http://i1.pepperfry.com/media/catalog/product/c/a/800x880/canon-red-recliner-by-alcanes-canon-red-recliner-by-alcanes-u6yqbb.jpg';
+echo remote_file_exists($file);
+echo remote_file_exists($file2);
